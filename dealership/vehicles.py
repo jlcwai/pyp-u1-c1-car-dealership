a271789f3e63cc33d4066287c6974da95fe4711f
jasonlimcheewai <jasonlimcheewai@gmail.com>
2018-07-21 02:52:59 -0400

    savings commits

--- dealership/vehicles.py
+++ dealership/vehicles.py
@@ -1,15 +1,28 @@
 class Vehicle(object):
-    def __init__(self, maker, model, year, base_price, miles):
-        pass
+    def __init__(self, SALE_PRICE_MULTIPLIER, PURCHASE_PRICE_MULTIPLIER, maker, model, year, base_price, miles):
+        self.maker = maker
+        self.model = model
+        self.year = year
+        self.base_price = base_price
+        self.miles = miles
 
 
 class Car(Vehicle):
-    pass
+    def __init__(self, SALE_PRICE_MULTIPLIER, PURCHASE_PRICE_MULTIPLIER, maker, model, year, base_price, miles):
+      super(Car, self).__init__(SALE_PRICE_MULTIPLIER, PURCHASE_PRICE_MULTIPLIER, maker, model, year, base_price, miles)
+      self.SALE_PRICE_MULTIPLIER = 1.2
+      self.PURCHASE_PRICE_MULTIPLIER = 0.004
 
 
 class Motorcycle(Vehicle):
-    pass
+    def __init__(self, SALE_PRICE_MULTIPLIER, PURCHASE_PRICE_MULTIPLIER, maker, model, year, base_price, miles):
+      super(Motorcycle, self).__init__(SALE_PRICE_MULTIPLIER, PURCHASE_PRICE_MULTIPLIER, maker, model, year, base_price, miles)
+      self.SALE_PRICE_MULTIPLIER = 1.1
+      self.PURCHASE_PRICE_MULTIPLIER = 0.009
 
 
 class Truck(Vehicle):
-    pass
+    def __init__(self, SALE_PRICE_MULTIPLIER, PURCHASE_PRICE_MULTIPLIER, maker, model, year, base_price, miles):
+      super(Truck, self).__init__(SALE_PRICE_MULTIPLIER, PURCHASE_PRICE_MULTIPLIER, maker, model, year, base_price, miles)
+      self.SALE_PRICE_MULTIPLIER = 1.6
+      self.PURCHASE_PRICE_MULTIPLIER = 0.02
